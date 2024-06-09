1fad233bb288c2eedd809782008c86908a4f2735
xob0t <32616886+xob0t@users.noreply.github.com>
2024-06-09 16:25:35 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='gp-uploader',
     version='0.0.6',
-    description='Script to monitor a directory and upload any new files to google photo using adb connected android device',
+    description='Script to monitor a directory and upload any existing files to google photo using adb connected android device',
     author='xob0t',
     url='https://github.com/xob0t/gp-uploader',
     packages=find_packages(),
