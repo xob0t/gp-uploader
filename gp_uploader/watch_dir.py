2d2141f75788b321b08de0d8c2852eea7a009ba3
xob0t <32616886+xob0t@users.noreply.github.com>
2024-06-30 11:27:28 +0300

    small tweaks, timeout arg

--- gp_uploader/watch_dir.py
+++ gp_uploader/watch_dir.py
@@ -18,15 +18,16 @@ class Adb_utils:
         cmd = self.device + ["exec-out", "uiautomator dump /dev/tty"]
         result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=True)
         # The output will contain some additional lines, so we need to extract the XML part
+        xml_start = result.stdout.find("<hierarchy")
         xml_end = result.stdout.find("UI hierchary dumped to")
-        xml_content = result.stdout[:xml_end]
+        xml_content = result.stdout[xml_start:xml_end]
         if not xml_content:
             raise Exception("uiautomator dump is empty")
         return xml_content
 
     def get_element_coordinates_by_xpath(self, xpath):
         xml_content = self._get_ui_hierarchy_dump()
-        # Parse the XML content
+
         xml_content_bytes = xml_content.encode("utf-8")
         # Parse the XML content
         tree = etree.parse(BytesIO(xml_content_bytes))
@@ -63,9 +64,10 @@ class Adb_utils:
 
 
 class Watcher:
-    def __init__(self, target_path, serial="", host_keep=False, log_uploads=False, log_level=""):
+    def __init__(self, target_path, serial="", timeout = None, host_keep=False, log_uploads=False, log_level=""):
         self.logger = self._new_logger(log_level)
         self.device = ["adb", "-s", serial] if serial else ["adb"]
+        self.timeout = timeout
         self.host_keep = host_keep
         self.log_uploads = log_uploads
         self.uploaded = self._get_uploaded()
@@ -74,6 +76,7 @@ class Watcher:
         self.upload_status = None  # used for toast monitoring
         self.adb_utils = Adb_utils(serial)
         self.upload_btn_coords = None
+        self.current_upload_filename = ""
 
     def _new_logger(self, log_level):
         logging.basicConfig(
@@ -85,13 +88,13 @@ class Watcher:
         return logging.getLogger("rich")
 
     def _wait_for_device(self):
-        self.logger.info("Waiting for device")
+        self.logger.info("waiting for device")
         while True:
             try:
                 # testing adb connection
                 cmd = self.device + ["shell", "getprop", "ro.product.model"]
                 device_model = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=True).stdout.strip()
-                self.logger.info(f"Device found: {device_model}")
+                self.logger.info(f"device found: {device_model}")
                 return
             except Exception as e:
                 self.logger.debug(e)
@@ -106,6 +109,8 @@ class Watcher:
             time.sleep(1)
             self.adb_utils.click_coordinates(self.upload_btn_coords)
             self.logger.info("waiting for status toast")
+
+            start_time = time.time()
             while True:
                 # Read a line from the events log
                 line = process.stdout.readline()
@@ -114,6 +119,9 @@ class Watcher:
                     return True
                 elif "Error, could not upload media" in line:
                     return False
+                if time.time() - start_time > self.timeout:
+                    self.logger.warning(f"{self.current_upload_filename} upload timeout reached")
+                    break
         except KeyboardInterrupt:
             pass
         finally:
@@ -121,23 +129,27 @@ class Watcher:
             process.terminate()
             process.wait()
 
-    def upload_files(self):
+    def _upload_files(self):
         self._wait_for_device()
         # get files
         files = [f for f in Path(self.target_path).rglob('*') if f.is_file()]
+        # gp does not like dotfiles sent via adb intets for some reason
+        files = [f for f in files if not f.name.startswith(".")]
         self.uploaded = self._get_uploaded()
         if not files:
             time_to_sleep = 30
-            self.logger.info(f"Empty dir, checking again in {time_to_sleep}s")
+            self.logger.info(f"empty dir, checking again in {time_to_sleep}s")
             time.sleep(time_to_sleep)
             return
         new_files = [file for file in files if file.name not in self.uploaded]
         if not new_files:
             time_to_sleep = 30
-            self.logger.info(f"No new files to upload, checking again in {time_to_sleep}s")
+            self.logger.info(f"no new files to upload, checking again in {time_to_sleep}s")
             time.sleep(time_to_sleep)
             return
         for file in new_files:
+            self._stop_photos()
+            self.current_upload_filename = file.name
             host_file_path = file
             device_file_path = Path.joinpath(self.device_media_path, file.name)
             try:
@@ -148,7 +160,7 @@ class Watcher:
     def watch(self):
         while True:
             try:
-                self.upload_files()
+                self._upload_files()
             except Exception as e:
                 self.logger.critical(e)
                 time.sleep(30)
@@ -165,14 +177,15 @@ class Watcher:
             self.upload_btn_coords = self.adb_utils.get_element_coordinates_by_xpath(upload_button_xpath)
         upload_status = self._start_upload()
         if upload_status is True:
-            self.logger.info(f"{device_file_path.name} upload complete")
-            self._save_as_uploaded(device_file_path.name)
+            self.logger.info(f"{self.current_upload_filename} upload complete")
+            if self.log_uploads:
+                self._save_as_uploaded(self.current_upload_filename)
             if not self.host_keep:
-                self.logger.info(f"{device_file_path.name} deleting from host")
+                self.logger.info(f"{self.current_upload_filename} deleting from host")
                 os.remove(host_file_path)
             self._delete_from_device(device_file_path)
         else:
-            self.logger.info(f"{device_file_path.name} upload error")
+            self.logger.info(f"{self.current_upload_filename} upload error")
 
     def _save_as_uploaded(self, filename):
         with open("uploaded.txt", "a", encoding="UTF-8") as file:
@@ -184,9 +197,14 @@ class Watcher:
         with open("uploaded.txt", "r", encoding="UTF-8") as file:
             lines = file.readlines()
         return [line.strip() for line in lines]
+    
+    def _stop_photos(self):
+        self.logger.debug("killing Photos app")
+        cmd = self.device + ["shell", "am", "force-stop", "com.google.android.apps.photos"]
+        subprocess.run(cmd, check=True)
 
     def _get_file_size_on_device(self, device_file_path):
-        self.logger.debug(f"{device_file_path.name} checking file size on device")
+        self.logger.debug(f"{self.current_upload_filename} checking file size on device")
         output = subprocess.run(self.device + ["shell", f'stat -c %s "{device_file_path}"'], capture_output=True, text=True, check=False)
         try:
             size = int(output.stdout.strip())
@@ -195,17 +213,17 @@ class Watcher:
         return size
 
     def _push_to_device(self, host_file_path, device_file_path):
-        self.logger.info(f"{device_file_path.name} Pushing to device")
+        self.logger.info(f"{self.current_upload_filename} pushing to device")
         cmd = self.device + ["push", host_file_path.as_posix(), device_file_path.as_posix()]
         subprocess.run(cmd, check=True)
         return device_file_path
 
     def _delete_from_device(self, device_file_path):
-        self.logger.info(f"{device_file_path.name} deleting from device")
+        self.logger.info(f"{self.current_upload_filename} deleting from device")
         subprocess.run(self.device + ["shell", f'rm "{device_file_path.as_posix()}"'], check=True)
 
     def _send_intent(self, device_file_path):
-        self.logger.info(f"{device_file_path.name} starting upload")
+        self.logger.info(f"{self.current_upload_filename} starting upload")
         uri = "file://" + quote(device_file_path.as_posix())
         process = subprocess.run(
             self.device + ["shell", "am", "start", "-a", "android.intent.action.SEND", "-t", "application/octet-stream",
@@ -225,12 +243,13 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("dir", type=str, help="Directory path to watch")
     parser.add_argument("-s", "--serial", type=str, help="Serial of the device to connect to")
+    parser.add_argument("-t", "--timeout", type=int, help="Upload timeout, seconds")
     parser.add_argument("-k", "--host-keep", action="store_true", help="Do not delete host files on successful upload")
     parser.add_argument("-u", "--log-uploads", action="store_true", help="Keep log of successful uploads in uploaded.txt")
     parser.add_argument("-l", "--log-level", choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"], default="INFO", help="Log level")
     args = parser.parse_args()
 
-    u = Watcher(args.dir, args.serial, args.host_keep, args.log_uploads, args.log_level)
+    u = Watcher(args.dir, args.serial, args.timeout, args.host_keep, args.log_uploads, args.log_level)
     u.watch()
 
 
